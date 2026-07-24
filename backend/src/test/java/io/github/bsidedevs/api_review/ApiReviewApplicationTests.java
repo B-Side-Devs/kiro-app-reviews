e01f3c03312170f8aa27d5cb6db04420be18d000
Denisa <denisa.gerez@gmail.com>
2026-07-23 21:49:04 -0300

    chore: update-dependencies

--- backend/src/test/java/io/github/bsidedevs/api_review/ApiReviewApplicationTests.java
+++ backend/src/test/java/io/github/bsidedevs/api_review/ApiReviewApplicationTests.java
@@ -2,9 +2,7 @@ package io.github.bsidedevs.api_review;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ApiReviewApplicationTests {
 
